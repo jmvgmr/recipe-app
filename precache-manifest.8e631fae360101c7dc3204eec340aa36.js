9468d7959100ec175a0cf3d0425a82abc1b8c366
jmvgmr <rmgvmj@gmail.com>
2020-01-30 18:41:28 +0100

    Updates

---
+++ precache-manifest.8e631fae360101c7dc3204eec340aa36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b0903d75c31ab812ddfb86889d35d6",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "d0b5ed5e530d5a3a77a1",
+    "url": "/recipe-app/static/css/main.257be312.chunk.css"
+  },
+  {
+    "revision": "137720e5425a7cc913d8",
+    "url": "/recipe-app/static/js/2.72223848.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-app/static/js/2.72223848.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0b5ed5e530d5a3a77a1",
+    "url": "/recipe-app/static/js/main.aa135b36.chunk.js"
+  },
+  {
+    "revision": "148e400df5fd6abd3a3f",
+    "url": "/recipe-app/static/js/runtime-main.5fe0ecde.js"
+  }
+]);
